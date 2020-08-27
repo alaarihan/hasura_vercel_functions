9b05f28c31785cb2ab6184ac891d8b8da73ed9b7
Alaa Rihan <alaa.addeen@gmail.com>
2020-08-27 19:29:50 +0300

    Make fetch one by one to avoid failure

--- api/keepMachineAwake.js
+++ api/keepMachineAwake.js
@@ -1,6 +1,12 @@
 const fetch = require("node-fetch");
 const { getMachines } = require("./lib/common");
 
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
 const keepMachineAwake = async (req, res) => {
   if (req.method !== "POST") {
     return res.status(403).send("Forbidden!");
@@ -25,8 +31,8 @@ const keepMachineAwake = async (req, res) => {
     return res.status(200).send({ message: `No active machines found!` });
   }
 
-  activeMachines.forEach((machine) => {
-    fetch(`${machine.url}${req.body.payload.path}`)
+  asyncForEach(activeMachines, async (machine) => {
+    await fetch(`${machine.url}${req.body.payload.path}`)
       .then((res) => res.json())
       .then((json) => console.log(json))
       .catch((error) => {
